928e7dd50936c0e651c44b8dad0ba46203596516
Natalie Muñoz <nataliemunozh@gmail.com>
2023-06-30 21:31:16 -0400

    base de datos definida

--- todolist/settings.py
+++ todolist/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nmunozh_todolist',
+        'USER': 'nmunozh_test',
+        'PASSWORD': 'Martina2023.',
+        'HOST': 'postgresql-nmunozh.alwaysdata.net',
+        'PORT': '5432',
     }
 }
 
