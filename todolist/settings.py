c9f0335eff23938d91810d2e85d49f8e8e4c8dff
Natalie Muñoz <nataliemunozh@gmail.com>
2023-07-01 11:23:49 -0400

    listo para desplegar

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%w-lwb_im7rtul5m%46daxt7avsyq8vv)i&(z2b*0umr!6qt48
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos',
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
